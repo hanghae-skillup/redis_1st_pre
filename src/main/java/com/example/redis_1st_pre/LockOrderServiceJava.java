710314a7794eddb2ef0c1e3293dad115d7976707
kimbro97 <rlagudwog@gmail.com>
2025-01-03 20:08:43 +0900

    feat: lock를 활용한 동시성 제어 작업

--- src/main/java/com/example/redis_1st_pre/LockOrderServiceJava.java
+++ src/main/java/com/example/redis_1st_pre/LockOrderServiceJava.java
@@ -1,16 +1,24 @@
 package com.example.redis_1st_pre;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class SyncOrderServiceJava {
+public class LockOrderServiceJava {
+
+    private static final Logger log = LoggerFactory.getLogger(LockOrderServiceJava.class);
+    Lock lock = new ReentrantLock();
 
     // 상품 DB
     private final Map<String, Integer> productDatabase = new HashMap<>();
     // 가장 최근 주문 정보를 저장하는 DB
     private final ThreadLocal<Map<String, OrderInfo>> latestOrderDatabase = ThreadLocal.withInitial(HashMap::new);
 
-    public SyncOrderServiceJava() {
+    public LockOrderServiceJava() {
         // 초기 상품 데이터
         productDatabase.put("apple", 100);
         productDatabase.put("banana", 50);
@@ -18,21 +26,26 @@ public class SyncOrderServiceJava {
     }
 
     // 주문 처리 메서드
-    public synchronized void order(String productName, int amount) {
-        Integer currentStock = productDatabase.getOrDefault(productName, 0);
-
+    public void order(String productName, int amount) {
+        lock.lock();
         try {
-            Thread.sleep(1); // 동시성 이슈 유발을 위한 인위적 지연
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            throw new RuntimeException(e);
-        }
+            Integer currentStock = productDatabase.getOrDefault(productName, 0);
 
-        if (currentStock >= amount) {
-            System.out.println("Thread " + Thread.currentThread().threadId() + " 주문 정보: ");
-            System.out.println("    " + productName + ": 1 건 ([" + amount + "])");
-            productDatabase.put(productName, currentStock - amount);
-            latestOrderDatabase.get().put(productName, new OrderInfo(productName, amount, System.currentTimeMillis()));
+            try {
+                Thread.sleep(1); // 동시성 이슈 유발을 위한 인위적 지연
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException(e);
+            }
+
+            if (currentStock >= amount) {
+                System.out.println("Thread " + Thread.currentThread().threadId() + " 주문 정보: ");
+                System.out.println("    " + productName + ": 1 건 ([" + amount + "])");
+                productDatabase.put(productName, currentStock - amount);
+                latestOrderDatabase.get().put(productName, new OrderInfo(productName, amount, System.currentTimeMillis()));
+            }
+        } finally {
+            lock.unlock();
         }
     }
 
