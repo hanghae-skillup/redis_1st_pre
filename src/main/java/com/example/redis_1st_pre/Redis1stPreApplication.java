dcce55f74bc4a9e0748fd02f2e9d4811be1e0a1c
kimbro97 <rlagudwog@gmail.com>
2025-01-03 19:56:28 +0900

    project init

---
+++ src/main/java/com/example/redis_1st_pre/Redis1stPreApplication.java
@@ -0,0 +1,13 @@
+package com.example.redis_1st_pre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Redis1stPreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Redis1stPreApplication.class, args);
+	}
+
+}
